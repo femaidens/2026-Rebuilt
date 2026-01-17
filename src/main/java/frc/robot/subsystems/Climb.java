107c00b6f76e1e736abe73677828e7fa7ee3b8c8
Lydia Chen <lydiachen15@gmail.com>
2026-01-17 14:02:57 -0500

    fields and reversemtr

---
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class Climb {
+    private final TalonFX leaderMotor;
+    private final TalonFX followerMotor;
+
+
+}
