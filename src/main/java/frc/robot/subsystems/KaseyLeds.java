0212f313f526433ba4e536fe22b2f91b6b30fc4a
yujaime <yuj@bxscience.edu>
2026-01-24 16:41:03 -0500

    kasey does code for once

--- src/main/java/frc/robot/subsystems/KaseyLeds.java
+++ src/main/java/frc/robot/subsystems/KaseyLeds.java
@@ -1,5 +1,18 @@
 package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.AddressableLED.ColorOrder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//import edu.wpi.feedback.led.Color;
+// import com.ctre.lib.feedback.led.LEDStrip;
 
 public class KaseyLeds {
-    
+    private final int BUFFER_LENGTH = 67;
+    private final AddressableLED leds; 
+    private final AddressableLED buffer; //led buffer
+
+    public KaseyLeds () {
+        leds = new AddressableLED();
+
+    }
 }
