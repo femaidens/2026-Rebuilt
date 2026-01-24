c7e62d5592851801825615571fc62021c9fdf95c
Kailey Liou <kaileyliou09@gmail.com>
2026-01-24 16:26:50 -0500

    started imports fields and constructor

--- src/main/java/frc/robot/subsystems/KaileyLeds.java
+++ src/main/java/frc/robot/subsystems/KaileyLeds.java
@@ -1,5 +1,19 @@
 package frc.robot.subsystems;
 
-public class KaileyLeds {
-    
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.util.Color;
+
+public class KaileyLeds extends SubsystemBase{
+    private final AddressableLED lights;
+    private final AddressableLEDBuffer buffer;
+
+    public KaileyLeds() {
+        lights = new AddressableLED(Ports.LedPorts.LED_PORT);
+        buffer = new AddressableLEDBuffer(Constants.LedConstants.LED_LENGTH);
+    }
+
 }
