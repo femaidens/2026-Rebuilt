d298096dfb263bce39a8307f1da6825bf3ce7fd3
Lydia Chen <lydiachen15@gmail.com>
2026-01-28 17:50:10 -0500

    added color constants and void methods

--- src/main/java/frc/robot/subsystems/LydiaLeds.java
+++ src/main/java/frc/robot/subsystems/LydiaLeds.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.AddressableLEDBufferView;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Ports;
@@ -10,12 +12,28 @@ import frc.robot.Ports;
 public class LydiaLeds extends SubsystemBase {
     private final AddressableLED ledLight;
     private final AddressableLEDBuffer ledBuffer;
-    // private final AddressableLEDBufferView ledBufferView;
+    private final AddressableLEDBufferView ledBufferView;
+
+    private final LEDPattern firstRed;
+    private final LEDPattern firstBlue;
+    private final LEDPattern bumperRed;
+    private final LEDPattern bumperBlue;
+    private final LEDPattern green;
+    private final LEDPattern purple;
+
 
     public LydiaLeds() {
         ledLight = new AddressableLED(Ports.LedPorts.LYDIA_LED_PORT);
         ledBuffer = new AddressableLEDBuffer(Constants.LedConstants.LYDIA_LED_LENGTH);
-        // ledBufferView = new AddressableLEDBufferView(null, 0, 0);
+        ledBufferView = ledBuffer.createView(0,41);
+
+        firstRed = LEDPattern.solid(Color.kFirstRed);
+        firstBlue = LEDPattern.solid(Color.kFirstBlue);
+        bumperRed = LEDPattern.solid(Color.kFirebrick);
+        bumperBlue = LEDPattern.solid(Color.kMediumBlue);
+        green = LEDPattern.solid(Color.kGreenYellow);
+        purple = LEDPattern.solid(Color.kDarkViolet);
+
 
         ledLight.setLength(ledBuffer.getLength());
 
@@ -23,10 +41,39 @@ public class LydiaLeds extends SubsystemBase {
         ledLight.start();
 
     }
+
+    public Command setSolidColor() {
+        return 
+    }
+
+    public void setFirstRed() {
+        firstRed.applyTo(ledBuffer);
+    }
+
+    public void setFirstBlue() {
+        firstBlue.applyTo(ledBuffer);
+    }
+
+    public void setBumperRed() {
+        bumperRed.applyTo(ledBuffer);
+    }
+
+     public void setBumperBlue() {
+        bumperBlue.applyTo(ledBuffer);
+    }
+
+     public void setGreen() {
+        green.applyTo(ledBuffer);
+    }
+
+     public void setPurple() {
+        purple.applyTo(ledBuffer);
+    }
     
     @Override
     public void periodic() {
         ledLight.setData(ledBuffer);
+        //highk dont know if i need this
     }
 
 }
