8a0f2924f6099603815b14b2c0e0abacb98e5f26
Tsering Lama <phinjohyolmo@phinjos-MacBook-Pro.local>
2026-01-31 11:18:44 -0500

    does import work

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,10 +21,10 @@ public final class Constants {
   public static class ShooterConstants{
     public static final int CURRENT_LIMIT = 0;
     public static final double SHOOTER_MOTOR_SPEED = 0;
-    public static final double ANGLE_MOTOR_SPEED = 0;
+    public static final double ANGLE_MOTOR_SPEED = 0.7;
     public static CANBus CANBUS = new CANBus("rio");
       public static class PIDConstants {
-        public static final double kP = 0;
+        public static final double kP = 2;
         public static final double kI = 0;
         public static final double kD = 0;
      }
