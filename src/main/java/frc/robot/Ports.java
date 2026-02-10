37875808d50b6bcf7d855e5e76ba3471d31359fd
lydia chen <lydiachen15@gmail.com>
2026-02-10 17:42:51 -0500

    updated ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -2,8 +2,8 @@ package frc.robot;
 
 public class Ports {
     public static final class ClimbPorts {
-        public static final int LEADER_MOTOR = 1;
-        public static final int FOLLOWER_MOTOR = 2;
+        public static final int LEADER_MOTOR = 15;
+        public static final int FOLLOWER_MOTOR = 14;
         public static final int BOTTOM_LIMIT_SWITCH = 3;
     }
 }
