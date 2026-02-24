1b1ea0ecf0ef0c5017c32ead40058b2eb370c0f6
Kailey Liou <kaileyliou09@gmail.com>
2026-02-24 16:34:28 -0500

    removed level two and three commands

--- src/main/java/frc/robot/commands/ClimbingL1.java
+++ src/main/java/frc/robot/commands/ClimbingL1.java
@@ -1,5 +1,26 @@
 package frc.robot.commands;
+import frc.robot.Constants;
+import frc.robot.subsystems.Climb;
+import edu.wpi.first.wpilibj2.command.Command;
 
 public class ClimbingL1 {
-    
+    private Climb climb;
+
+    public Climbing(Climb climb) {
+        this.climb = climb;
+    }
+
+    public Command levelOneExtend() {
+        return climb.setLevelCmd(Constants.ClimbConstants.SetpointConstants.LEVEL_ONE, true);
+    }
+
+    public Command levelOneRetract() {
+        return climb.setLevelCmd(Constants.ClimbConstants.SetpointConstants.MINIMUM, false);
+    }
+
+    public Command climbOneCmd() {
+        return levelOneExtend()
+            .andThen(levelOneRetract());
+    }
+
 }
