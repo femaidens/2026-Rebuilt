3ced701e8a6889c01fd2320d498d05bc465299c9
Jacqueline Ni <jacquelineni@Jacquelines-MacBook-Air.local>
2026-03-18 21:50:32 -0400

    fixed auton code

---
+++ src/main/java/frc/robot/autos/ShootPreloaded.java
@@ -0,0 +1,24 @@
+package frc.robot.autos;
+
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.Shooting;
+import frc.robot.subsystems.AutoShooter;
+import frc.robot.subsystems.Hopper;
+
+public class ShootPreloaded extends SequentialCommandGroup {
+    public ShootPreloaded(Shooting shooting, AutoShooter autoshooter, Hopper hopper){
+        addCommands(
+            autoshooter.runShooterMotorCmd().withTimeout(3),
+            
+            Commands.parallel(
+                autoshooter.runShooterMotorCmd(),
+                shooting.prepareShotAuto(hopper, autoshooter)
+            ).withTimeout(3),
+
+            autoshooter.stopShooterMotorCmd(),
+            hopper.stopSpindexer(),
+            autoshooter.stopIndexerMotorCmd()
+        );
+    }
+}
